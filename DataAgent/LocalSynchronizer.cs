a897a22d98013c6bf1ee1a359e1d685c979e61f5
Khaled Sharaf Eldin <wi15b189@technikum-wien.at>
2018-06-24 19:07:25 +0200

    merge

--- DataAgent/LocalSynchronizer.cs
+++ DataAgent/LocalSynchronizer.cs
@@ -75,7 +75,7 @@ namespace LocalSynchronization
                 {
                     SetConnectionStatus(false);
                 }
-                Thread.Sleep(10000);
+                Thread.Sleep(60000);
             }
         }
 
